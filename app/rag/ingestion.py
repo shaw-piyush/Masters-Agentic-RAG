2de78018f9394704d34281a726dd3153745809e2
unknown <shawpiyush2001@gmail.com>
2026-03-14 12:52:40 +0530

    Chunking the data

--- app/rag/ingestion.py
+++ app/rag/ingestion.py
@@ -1,10 +1,35 @@
+import os
 from langchain_community.document_loaders import PyMuPDFLoader, DirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from app.utils.config import CHROMA_PERSIST_DIR, CHUNK_OVERLAP, CHUNK_SIZE
 from app.rag.embedder import get_embeddings
+import pymupdf
+import chromadb
 
-def ingest_docs(data_dir : str = "./data/raw"):
+def clean_data(data_dir : str = "./data/raw", op_dir : str = "./data/cleaned"):
+
+    for filename in os.listdir(data_dir):
+        if filename.lower().endswith('.pdf'):
+            input_path = os.path.join(data_dir, filename)
+            output_path = os.path.join(op_dir, f"repaired_{filename}")
+
+            try:
+                # PyMuPDF automatically attempts to repair structure on open
+                doc = pymupdf.open(input_path)
+                
+                doc.save(output_path, garbage=3, deflate=True, clean=True)
+                doc.close()
+                
+                print(f"Successfully repaired: {filename}")
+                
+            except Exception as e:
+                print(f"Failed to repair {filename}: {e}")
+
+# clean_data()
+
+
+def ingest_docs(data_dir : str = "./data/cleaned"):
 
     loader = DirectoryLoader(data_dir, glob="**/*.pdf", loader_cls= PyMuPDFLoader)
 
@@ -25,5 +50,17 @@ def ingest_docs(data_dir : str = "./data/raw"):
 
     print(f"Ingested {len(chunks)} chunks into ChromaDB")
     return stores
+
+def show_collection():
+    client = chromadb.PersistentClient(path="./stores")
+    collection = client.get_collection("langchain")
+    print(collection)
+    
+    results = collection.get(include=["documents", "metadatas"])
+    
+    for i, doc in enumerate(results["documents"]):         ## Show all the chunks
+        print(f"\n--- Chunk {i+1} ---")
+        print(f"Source: {results['metadatas'][i]}")
+        print(f"Content: {doc[:200]}...")
 
     
--- scripts/ingest_docs.py
+++ scripts/ingest_docs.py
@@ -3,9 +3,11 @@ import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from app.rag.ingestion import ingest_docs
+from app.rag.ingestion import ingest_docs, clean_data, show_collection
 
 if __name__ == "__main__":
     print("Starting document ingestion...")
-    ingest_docs("./data/raw")
+    clean_data("./data/raw", "./data/cleaned")
+    ingest_docs("./data/cleaned")
+    show_collection()
     print("Done!")
